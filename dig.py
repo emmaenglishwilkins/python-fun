64b5aa024319921803db2d090b3ad067dcea6647
emmaenglishwilkins <78217522+emmaenglishwilkins@users.noreply.github.com>
2025-04-01 15:38:59 -0400

    done ?

--- dig.py
+++ dig.py
@@ -51,16 +51,38 @@ def show_map(t_map):
             t_map[row][col].goto(col*42 - 142, row*42 -142)
 
 def dig():
-    global m, goal_row, goal_col
+    global m, goal_row, goal_col, limit, count
     for row in m:
         for square in row:
-            if player.distance(square) < 15:
+            if player.distance(square) < 15 and limit > 0:
                 square.color("chocolate")
                 c = row.index(square)
                 r = m.index(row)
+                if goal_row  == r or goal_col == c:
+                    square.color("saddlebrown")
+                limit -= 1
+                if limit == 0:
+                    print("You LOSE")
+                    m[goal_row][goal_col].color("red")
+                    done()
                 if goal_row  == r and goal_col == c:
                     square.color("gold")
                     square.shape("circle")
+                    print("you found treasure!")
+                    count += 1
+                    win = True
+                    time.sleep(2)
+                    goal_row = random.randint(0,6)
+                    goal_col = random.randint(0,6)
+                    for row in m:
+                        for t in row:
+                            t.color("green")
+                            t.shape("square")
+                    limit = 20
+                    win = False
+                    
+
+                    
 def main():
     global m, goal_row, goal_col
     m = create_map()
@@ -68,6 +90,9 @@ def main():
     player.goto(0,0)
     goal_row = random.randint(0,6)
     goal_col = random.randint(0,6)
+    limit = 20
+    count = 0
+    win = False
 
     screen.onkey(up, "Up")
     screen.onkey(down, "Down")
